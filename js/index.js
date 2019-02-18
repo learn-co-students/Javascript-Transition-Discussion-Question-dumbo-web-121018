e78f84495e0868d882a0151a6172b640e376957a
milespucarelli <miles.pucarelli@gmail.com>
2019-02-18 16:05:22 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,35 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
+const lincolnButton = document.querySelector('#save_lincoln')
+const foreground = document.querySelector('#foreground')
+
+function fadeOut (interval) {
+  const newInterval = interval / 100
+  setInterval(function () {
+    if (!foreground.style.opacity) {
+      foreground.style.opacity = 1
+    }
+    if (foreground.style.opacity > 0) {
+      foreground.style.opacity -= 0.01
+    }
+  }, newInterval)
+}
+
+function saveLincoln (interval) {
+  if (isNaN(interval)) {
     interval = 10000
   }
-  $('#foreground').fadeOut(interval);
+  fadeOut(interval)
 }
+
+lincolnButton.addEventListener('click', () => {
+  const intervalInput = parseInt(document.querySelector('#interval').value)
+  saveLincoln(intervalInput)
+})
